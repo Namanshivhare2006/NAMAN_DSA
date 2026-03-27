b028fbfa1ed4594e6078e0b39791c97796a1c911
Namanshivhare2006 <namanshivhare04@gmail.com>
2026-03-27 18:44:20 +0530

    Time: 4 ms (77.21%) | Memory: 43.5 MB (51.25%) - LeetSync

---
+++ 1137-height-checker/height-checker.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        // int[] sorted = new int[heights.length];
+        // for(int i =0; i < heights.length; i++) {
+        //     sorted[i] = heights[i];
+        // }
+    //    int[] sorted = Arrays.copyOf(heights, heights.length);
+            int[] sorted  = heights.clone();
+
+        Arrays.sort(sorted);
+
+        int total = 0;
+        for(int i =0; i < heights.length; i++) {
+            if(sorted[i] != heights[i]) total++;
+        }
+
+        return total;
+    }
+}
