7866a6a0efe8a6869b9220a16e567e2492bb6b3f
Namanshivhare2006 <namanshivhare04@gmail.com>
2026-03-30 16:48:06 +0530

    Time: 1 ms (96.45%) | Memory: 42.3 MB (95.83%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,7 +1,13 @@
+// class Solution {
+//     public boolean isPowerOfTwo(int n) {
+//         if(n<=0) return false;
+//         if(n==1) return true;
+//         return (n%2==0) && isPowerOfTwo(n/2);
+//     }
+// }
 class Solution {
-    public boolean isPowerOfTwo(int n) {
+    public boolean isPowerOfTwo(int n){
         if(n<=0) return false;
-        if(n==1) return true;
-        return (n%2==0) && isPowerOfTwo(n/2);
+        return (n & (n - 1)) == 0;
     }
 }
