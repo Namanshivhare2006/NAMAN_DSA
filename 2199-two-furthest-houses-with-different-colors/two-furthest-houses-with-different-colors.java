5a2f7759e5ea203c3bc6a6442ec1b4580e1d428e
Namanshivhare2006 <namanshivhare04@gmail.com>
2026-04-20 15:50:16 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (80.83%) - LeetSync

--- 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.java
+++ 2199-two-furthest-houses-with-different-colors/two-furthest-houses-with-different-colors.java
@@ -1,24 +1,47 @@
+// class Solution {
+//     public int maxDistance(int[] colors) {
+//         int n = colors.length;
+//         int max = 0;
+
+//         // Compare with first element
+//         for(int i = n - 1; i >= 0; i--){
+//             if(colors[i] != colors[0]){
+//                 max = Math.max(max, i);
+//                 break;
+//             }
+//         }
+
+//         // Compare with last element
+//         for(int i = 0; i < n; i++){
+//             if(colors[i] != colors[n - 1]){
+//                 max = Math.max(max, n - 1 - i);
+//                 break;
+//             }
+//         }
+
+//         return max;
+//     }
+// }
 class Solution {
     public int maxDistance(int[] colors) {
         int n = colors.length;
-        int max = 0;
-
-        // Compare with first element
-        for(int i = n - 1; i >= 0; i--){
-            if(colors[i] != colors[0]){
-                max = Math.max(max, i);
-                break;
-            }
+        
+        int left = 0;
+        int right = n - 1;
+        
+        // Fix left, move right
+        while(colors[left] == colors[right]) {
+            right--;
         }
-
-        // Compare with last element
-        for(int i = 0; i < n; i++){
-            if(colors[i] != colors[n - 1]){
-                max = Math.max(max, n - 1 - i);
-                break;
-            }
+        int ans = right - left;
+        
+        // Fix right, move left
+        left = 0;
+        while(colors[left] == colors[n - 1]) {
+            left++;
         }
-
-        return max;
+        ans = Math.max(ans, (n - 1) - left);
+        
+        return ans;
     }
 }
