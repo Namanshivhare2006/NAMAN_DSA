1938d255da440a750d64cb0c7af2056aaef1a4f0
Namanshivhare2006 <namanshivhare04@gmail.com>
2026-04-30 09:56:21 +0530

    Time: 2 ms (94.19%) | Memory: 56.7 MB (89.70%) - LeetSync

---
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int goodNodes(TreeNode root) {
+        return helper(root, root.val);
+    }
+
+    public int helper(TreeNode root, int max) {
+        if (root == null) return 0;
+        int count = 0;
+        if (root.val >= max) {
+            count++;
+        }
+        max = Math.max(max, root.val);
+
+        count += helper(root.left, max);
+        count += helper(root.right, max);
+
+        return count;
+    }
+}
