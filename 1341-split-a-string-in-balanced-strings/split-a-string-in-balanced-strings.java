064813057e6f0fb2b86c9117dde70aedc774b3e6
Namanshivhare2006 <namanshivhare04@gmail.com>
2026-06-14 17:08:21 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (40.90%) - LeetSync

---
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int c =0;
+        int ch = 0;
+        for(int i = 0; i<s.length(); i++){
+            if(s.charAt(i)=='R'){
+                ch++;
+
+            }
+            else{
+                ch--;
+            }
+            if(ch==0){
+                c++;
+            }
+        }
+        return c;
+    }
+}
