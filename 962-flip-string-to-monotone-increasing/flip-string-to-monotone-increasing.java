efd54a2c649e0579587219c4e090254c867ce54d
Namanshivhare2006 <namanshivhare04@gmail.com>
2026-06-29 00:47:08 +0530

    Time: 108 ms (6.47%) | Memory: 74.1 MB (8.24%) - LeetSync

---
+++ 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int minFlipsMonoIncr(String s) {
+        int n = s.length();
+        int dp[][] = new int[n][2];
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<2; j++){
+                dp[i][j] = -1;
+            }
+        }
+        return solve(s,n,0,0,dp);
+    }
+    public int solve(String s,int n,int curr, int prev, int dp[][]){
+        if(curr>=n){
+            return 0;
+        }
+        if(dp[curr][prev]!=-1){
+            return dp[curr][prev];
+        }
+        int flip = Integer.MAX_VALUE;
+        int noflip = Integer.MAX_VALUE;
+
+        if(s.charAt(curr)=='0'){
+             if(prev==1){
+                flip = 1+solve(s,n,curr+1,1,dp);
+             }else{
+                flip = 1 + solve(s,n,curr+1,1,dp);
+                noflip = solve(s,n,curr+1,0,dp);
+             }
+        }else if(s.charAt(curr)=='1'){
+              if(prev==1){
+                noflip = solve(s,n,curr+1,1,dp);
+             }else{
+                flip = 1 + solve(s,n,curr+1,0,dp);
+                noflip = solve(s,n,curr+1,1,dp);
+             }
+        }
+        return dp[curr][prev] = Math.min(flip,noflip);
+
+        }
+    }
