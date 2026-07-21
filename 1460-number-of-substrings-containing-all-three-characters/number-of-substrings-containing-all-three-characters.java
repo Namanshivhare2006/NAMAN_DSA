8fa585ecf5c60b28b73bd56621be966c43297bd6
Namanshivhare2006 <namanshivhare04@gmail.com>
2026-07-21 18:10:42 +0530

    Time: 15 ms (44.73%) | Memory: 46.1 MB (90.55%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -1,11 +1,32 @@
+// class Solution {
+//     public int numberOfSubstrings(String s) {
+//         int[]lastChar={-1,-1,-1};
+//         int ans=0;
+//         for(int i=0;i<s.length();i++){
+//             lastChar[s.charAt(i)-'a']=i;
+//             ans+=1+Math.min(lastChar[0],Math.min(lastChar[1],lastChar[2]));
+//         }
+//         return ans;
+//     }
+// }
 class Solution {
     public int numberOfSubstrings(String s) {
-        int[]lastChar={-1,-1,-1};
-        int ans=0;
-        for(int i=0;i<s.length();i++){
-            lastChar[s.charAt(i)-'a']=i;
-            ans+=1+Math.min(lastChar[0],Math.min(lastChar[1],lastChar[2]));
+        int n = s.length();
+
+        int[] freq = new int[3];
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < n; right++) {
+            freq[s.charAt(right) - 'a']++;
+
+            while (freq[0] > 0 && freq[1] > 0 && freq[2] > 0) {
+                ans += (n - right);
+                freq[s.charAt(left) - 'a']--;
+                left++;
+            }
         }
+
         return ans;
     }
 }
