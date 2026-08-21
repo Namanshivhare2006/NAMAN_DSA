8d756474f03f9a91273410c2b14ac7b19c1367dd
Namanshivhare2006 <namanshivhare04@gmail.com>
2026-08-21 23:41:25 +0530

    Time: 74 ms (54.31%) | Memory: 47.1 MB (19.31%) - LeetSync

---
+++ 459-repeated-substring-pattern/repeated-substring-pattern.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        String str = s + s;
+        String str1 = str.substring(1,str.length() - 1);
+        return str1.contains(s);
+
+    }
+}
