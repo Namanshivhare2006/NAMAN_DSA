a3860aa92599a4f890f1bb256b693c1ffb349896
Namanshivhare2006 <namanshivhare04@gmail.com>
2026-08-27 13:17:50 +0530

    Time: 45 ms (75.00%) | Memory: 48 MB (56.18%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
@@ -1,23 +1,65 @@
+// class Solution {
+//     public String sortVowels(String s) {
+//      List<Character> list = new ArrayList<>();
+
+//        for (char ch : s.toCharArray()) {
+//             if ("AEIOUaeiou".indexOf(ch) != -1) {
+//                 list.add(ch);
+//             }
+//         }
+//     Collections.sort(list);
+//      char[] arr = s.toCharArray();
+
+//         int index = 0;
+
+//         // Step 3: Replace vowels with sorted vowels
+//         for (int i = 0; i < arr.length; i++) {
+
+//             if ("AEIOUaeiou".indexOf(arr[i]) != -1) {
+//                 arr[i] = list.get(index);
+//                 index++;
+//             }
+//         }
+
+//         return new String(arr);
+//     }
+// }
+import java.util.*;
+
 class Solution {
     public String sortVowels(String s) {
-     List<Character> list = new ArrayList<>();
 
-       for (char ch : s.toCharArray()) {
-            if ("AEIOUaeiou".indexOf(ch) != -1) {
-                list.add(ch);
+        List<Character> vowels = new ArrayList<>();
+
+        // Fetch vowels manually
+        for (char ch : s.toCharArray()) {
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' ||
+                ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' ||
+                ch == 'O' || ch == 'U') {
+
+                vowels.add(ch);
             }
         }
-    Collections.sort(list);
-     char[] arr = s.toCharArray();
 
-        int index = 0;
+        // Sort vowels
+        Collections.sort(vowels);
 
-        // Step 3: Replace vowels with sorted vowels
+        char[] arr = s.toCharArray();
+        int j = 0;
+
+        // Put sorted vowels back
         for (int i = 0; i < arr.length; i++) {
 
-            if ("AEIOUaeiou".indexOf(arr[i]) != -1) {
-                arr[i] = list.get(index);
-                index++;
+            char ch = arr[i];
+
+            if (ch == 'a' || ch == 'e' || ch == 'i' ||
+                ch == 'o' || ch == 'u' ||
+                ch == 'A' || ch == 'E' || ch == 'I' ||
+                ch == 'O' || ch == 'U') {
+
+                arr[i] = vowels.get(j++);
             }
         }
 
